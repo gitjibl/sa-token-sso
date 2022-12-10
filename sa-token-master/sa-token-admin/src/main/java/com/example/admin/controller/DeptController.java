c41491edb05790288caf749ab0bfbec3a981e2ee
桓一 <41766129+huanby@users.noreply.github.com>
2022-12-10 16:54:35 +0800

    添加部门管理

--- sa-token-master/sa-token-admin/src/main/java/com/example/admin/controller/DeptController.java
+++ sa-token-master/sa-token-admin/src/main/java/com/example/admin/controller/DeptController.java
@@ -1,12 +1,16 @@
 package com.example.admin.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.admin.domain.SysDept;
 import com.example.admin.service.SysDeptService;
+import com.example.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 系统部门管理
@@ -27,9 +31,33 @@ public class DeptController {
      * @param sysDept
      * @return
      */
-    @RequestMapping("/getList")
+    @RequestMapping("/getPageList")
     @ResponseBody
-    public IPage getList(SysDept sysDept) {
-        return sysDeptService.getPageList(sysDept);
+    public R getPageList(SysDept sysDept) {
+        IPage pageList = sysDeptService.getPageList(sysDept);
+        return R.ok(pageList);
     }
+
+    @GetMapping("/insert")
+    public R insert(SysDept sysDept) {
+        boolean update = sysDeptService.save(sysDept);
+        return update ? R.ok() : R.fail();
+    }
+
+    @GetMapping("/update")
+    public R update(SysDept sysDept) {
+        UpdateWrapper<SysDept> wrapper = new UpdateWrapper<>();
+        wrapper.eq("dept_id", sysDept.getDeptId());
+        boolean update = sysDeptService.update(sysDept, wrapper);
+        return update ? R.ok() : R.fail();
+    }
+
+    @PostMapping("/delete")
+    public R insert(@RequestBody String ids) {
+        List<String> list = JSON.parseArray(ids, String.class);
+        boolean update = sysDeptService.removeByIds(list);
+        return update ? R.ok() : R.fail();
+    }
+
+
 }
