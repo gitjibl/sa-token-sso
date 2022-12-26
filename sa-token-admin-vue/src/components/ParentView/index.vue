5e233faa4f2eeb97e6e691292c5e4caca37984e1
拿来吧你 <871048617@qq.com>
2022-12-26 17:55:32 +0800

    动态路由

---
+++ sa-token-admin-vue/src/components/ParentView/index.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Descripttion: 
+ * @Author: jibl
+ * @Date: 2022-08-31 10:02:03
+ * @LastEditors: jibl
+ * @LastEditTime: 2022-12-26 17:39:08
+-->
+<template>
+  <router-view />
+</template>
+<script>
+export default {
+  name: "ParentView",
+};
+</script>
